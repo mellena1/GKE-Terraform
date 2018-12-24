f3453d4a48c9e07ebfba08838bc3f2d5655f7287
Andrew Mellen <andrew_mellen@icloud.com>
2018-12-23 23:30:58 -0500

    Update post-install script

--- post-install.sh
+++ post-install.sh
@@ -8,13 +8,15 @@ fi
 
 # Get vars from terraform output
 echo "Retrieving zone and name from tf output..."
+terraform init > /dev/null
 ZONE=$(terraform output -json | jq -r .zone.value)
 NAME=$(terraform output -json | jq -r .cluster_name.value)
 
 # Get kubeconfig if it doesn't exist
 if [ ! -f ~/.kube/configs/andrewmellenorg-gke ]; then
     echo "Getting cluster kubeconfig from gcloud..."
-    KUBECONFIG=~/.kube/configs/andrewmellenorg-gke gcloud container clusters get-credentials $NAME --zone $ZONE &> /dev/null
+    gcloud config set project andrewmellenorg &> /dev/null
+    KUBECONFIG=~/.kube/configs/andrewmellenorg-gke gcloud container clusters get-credentials $NAME --zone $ZONE > /dev/null
 fi
 
 # Use sed to do replacement
