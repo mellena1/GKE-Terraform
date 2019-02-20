b46eef420c11685f1f10c8d2993af6bd4635bdc3
mellena1 <mellena1@wit.edu>
2019-02-19 20:07:35 -0500

    No more ingress node, get rid of logging and monitoring

--- gke.tf
+++ gke.tf
@@ -9,6 +9,9 @@ resource "google_container_cluster" "k8s-cluster" {
     password = ""
   }
 
+  logging_service = "none"
+  monitoring_service = "none"
+
   addons_config {
     kubernetes_dashboard {
       disabled = true
@@ -20,46 +23,6 @@ resource "google_container_cluster" "k8s-cluster" {
   #   name = "default-pool"
   #   node_count = "0"
   # }
-}
-
-resource "google_compute_address" "static-ip" {
-  name = "ipv4-address"
-}
-
-resource "google_container_node_pool" "ingress" {
-  name       = "ingress"
-  zone       = "us-east1-b"
-  cluster    = "${google_container_cluster.k8s-cluster.name}"
-  node_count = 1
-  version = "1.11.5-gke.5"
-
-  management = {
-    auto_repair  = true
-    auto_upgrade = false
-  }
-
-  node_config {
-    preemptible  = false
-    machine_type = "f1-micro"
-    disk_size_gb = 20
-
-    taint = {
-      key    = "ingress"
-      value  = "true"
-      effect = "NO_EXECUTE"
-    }
-
-    labels = {
-      ingress = "true"
-    }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
 }
 
 resource "google_container_node_pool" "worker-n1-standard-2" {
@@ -94,8 +57,3 @@ output "cluster_name" {
 output "zone" {
   value = "${google_container_cluster.k8s-cluster.zone}"
 }
-
-output "static-ip" {
-  value = "${google_compute_address.static-ip.address}"
-}
-
